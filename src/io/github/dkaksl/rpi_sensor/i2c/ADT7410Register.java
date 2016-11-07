4509f474a176cf66aad2e70b8eb3a2c045057b20
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-07 23:46:40 +0900

    added ADT7410 register enum skeleton

---
+++ src/io/github/dkaksl/rpi_sensor/i2c/ADT7410Register.java
@@ -0,0 +1,16 @@
+/** 
+ * 
+ * Copyright (C) 2016 Amani Kilumanga
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+package io.github.dkaksl.rpi_sensor.i2c;
+
+/**
+ * @author Amani Kilumanga
+ *
+ */
+public enum ADT7410Register {
+
+}
