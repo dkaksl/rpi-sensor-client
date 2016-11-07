4190268a40f512360b4bb6e2dac3ce4be4225a1d
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-07 23:46:28 +0900

    added ADT7410 class skeleton

---
+++ src/io/github/dkaksl/rpi_sensor/hardware/ADT7410.java
@@ -0,0 +1,16 @@
+/** 
+ * 
+ * Copyright (C) 2016 Amani Kilumanga
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+package io.github.dkaksl.rpi_sensor.hardware;
+
+/**
+ * @author Amani Kilumanga
+ *
+ */
+public class ADT7410 {
+
+}
