78e40570279c9f3f06bfa6792067e84e587b47f5
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-08 20:38:49 +0900

    added i2c register addresses

--- src/io/github/dkaksl/rpi_sensor/i2c/ADT7410Register.java
+++ src/io/github/dkaksl/rpi_sensor/i2c/ADT7410Register.java
@@ -12,5 +12,18 @@ package io.github.dkaksl.rpi_sensor.i2c;
  *
  */
 public enum ADT7410Register {
+	B00(0x48),
+	B01(0x49),
+	B10(0x4A),
+	B11(0x4B);
 
+	private final int address;
+
+	private ADT7410Register(int address) {
+		this.address = address;
+	}
+
+	public int getAddress() {
+		return address;
+	}
 }
